c0e04e4e858b5274e565fe89dddeb1d483a0455c
trokolisz <dani.csehovics@gmail.com>
2024-04-10 00:30:12 +0200

    login megvan

--- Client/blog/login/urls.py
+++ Client/blog/login/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name="j"),
     path('login/', views.login)
 ]
--- Client/blog/login/views.py
+++ Client/blog/login/views.py
@@ -1,11 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import User
+from .models import Topic, TopicType, Comment, User
 
 def index(request):
-    user =  User.objects.all()
+    user_id= request.GET.get('user', -1)
+    password = request.GET.get('pass', -1)
+    if user_id in [-1, ''] or password in [-1, ''] :
+         return render(request, 'login.html')
+    try:
+        #user_id=int(user_id)
+        
+            users = User.objects.get(username=user_id)
+        
+    except:
+        users = 1
+        
+    #return HttpResponse((users, password))
+    #comments = Comment.objects.all()
+    topics =  Topic.objects.filter()
     
-    return render(request, 'index.html', {'logins': user})
+    topic_types = TopicType.objects.all()
+    return render(request, 'index.html', {'topics': topics, "topic_types":topic_types})
 
 def login(request):
     return HttpResponse('Log in:')
+
+
+
+
